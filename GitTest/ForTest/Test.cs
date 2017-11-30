edf2ab9985ca5c5c81e19ede83ef64b5913b77ed
fly <2365833196@qq.com>
2017-11-30 16:11:47 +0800

    第二次修改

---
+++ GitTest/ForTest/Test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GitTest.ForTest
+{
+    public class Test
+    {
+        Int32 a { set; get; }
+    }
+}
